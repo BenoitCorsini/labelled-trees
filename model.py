530e325f01c92ceced00ad0c24e75362a4e170c6
BenoitCorsini <benoitcorsini@gmail.com>
2021-06-15 10:05:50 -0400

    model cleaned and described

--- model.py
+++ model.py
@@ -4,108 +4,96 @@ import numpy.random as npr
 
 class LabelledTree(object):
 
-    def __init__(self, n=None, nodes=None, edges=None, seed=None, **kwargs):
+    def __init__(self, n, seed=None, **kwargs):
         self.n = n
-        self._nodes = nodes
-        self._edges = edges
         self.seed = seed
-        npr.seed(self.seed)
 
-        if (self.n is None) & (self._nodes is None) & (self._edges is None):
-            raise Exception('No input in \'{}\' class'.format(self.__class__.__name__))
-
-        if self.n is None:
-            if self._nodes is None:
-                self.n = np.size(self._edges) + 1
-            else:
-                self.n = np.size(self._nodes) + 1
-
-        self.__nodes__()
-        self.__edges__()
         self.__tree__()
 
-    def __nodes__(self):
-        if self._nodes is None:
-            self._nodes = 1 + npr.randint(self.n, size=self.n - 1)
-        self._nodes = self._nodes.astype(int)
-        self.nodes = np.arange(1, self.n + 1)
-
-        if self.n != np.size(self._nodes) + 1:
-            raise Exception('\'nodes\' in \'{}\' of the wrong size: {}'.format(self.__class__.__name__, self._nodes))
-        elif (np.max(self._nodes) > self.n) or (np.min(self._nodes) < 1):
-            raise Exception('\'nodes\' in \'{}\' has wrong entries: {}'.format(self.__class__.__name__, self._nodes))
-
-    def __edges__(self):
-        if self._edges is None:
-            self._edges = 1 + np.argsort(npr.rand(self.n - 1))
-        self._edges = self._edges.astype(int)
-
-        if self.n != np.size(self._edges) + 1:
-            raise Exception('\'edges\' in \'{}\' of the wrong size: {}'.format(self.__class__.__name__, self._edges))
-        if np.any(self._edges - 1 != np.argsort(np.argsort(self._edges))):
-            raise Exception('\'edges\' in \'{}\' is not a permutation: {}'.format(self.__class__.__name__, self._edges))
-
     def __tree__(self):
+        '''
+        Creates the Cayley sequence and the corresponding line-breaking tree.
+        Contains the following arguments:
+        - self.cayley: a sequence of numbers in [n]^(n-1);
+        - self.leaves: the sequence of leaves of the tree;
+        - self.root: the root of the tree;
+        - self.n_leaves: the number of leaves;
+        - self.n_central_nodes: the number of nodes that are not leaves;
+        - self.edges: a list of the edges, ordered as (parent, child);
+        - self.paths: a dicionary where self.paths[node] is the path from the root to the node;
+        - self.children: a dictionary where self.children[node] is the list of children of the node.
+        '''
+        npr.seed(self.seed)
+        self.cayley = 1 + npr.randint(self.n, size=self.n - 1)
         self.leaves = np.arange(1, self.n + 1)
-        self.leaves = self.leaves[np.isin(self.leaves, self._nodes, invert=True)]
-        self.root = self._nodes[0]
+        self.leaves = self.leaves[np.isin(self.leaves, self.cayley, invert=True)]
+        self.root = self.cayley[0]
 
         self.n_leaves = len(self.leaves)
         self.n_central_nodes = self.n - self.n_leaves
 
-        self.edges = {}
-        self._path = {self.root : [self.root]}
+        self.edges = []
+        self.paths = {self.root : [self.root]}
         self.children = {}
-        tree_nodes = [self._nodes[0]]
-        leaf_index = 0
+        tree_nodes = [self.cayley[0]] # the nodes already explored
+        leaf_index = 0 # the index of the next leaf to consider
         for i in range(self.n - 1):
-            x = self._nodes[i]
-            y = self._nodes[(i+1) % (self.n - 1)]
-            e = self._edges[i]
+            x = self.cayley[i]
+            y = self.cayley[(i+1) % (self.n - 1)]
             if y in tree_nodes:
                 y = self.leaves[leaf_index]
                 leaf_index += 1
             else:
                 tree_nodes.append(y)
 
-            self.edges[x,y] = e
-            self._path[y] = self._path[x] + [y]
+            self.edges.append((x,y))
+            self.paths[y] = self.paths[x] + [y]
             if x not in self.children:
                 self.children[x] = []
             self.children[x].append(y)
 
+    def __iter__(self):
+        '''
+        When iterating through the tree, simply go through the values 1 to n.
+        '''
+        return iter(range(1, self.n + 1))
+
     def resample(self):
-        self._nodes = None
-        self._edges = None
+        '''
+        Resample a labelled tree of the same size.
+        '''
         if self.seed is not None:
             self.seed += 1
-        self.__nodes__()
-        self.__edges__()
         self.__tree__()        
 
     def degree(self, n):
+        '''
+        Computes the degree of the node n.
+        '''
         return len(self.children.get(n, []))
 
     def height(self, n):
+        '''
+        Computes the height of the node n.
+        '''
         return len(self._path[n]) - 1
 
     def parent(self, n):
+        '''
+        Computes the parent of the node n.
+        '''
         if n == self.root:
             return None
         else:
-            return self._path[n][-2]
+            return self.paths[n][-2]
 
     def path(self, n, edge_list=False):
+        '''
+        Computes the path from the root to n.
+        If edge_list = False, returns a list of the nodes.
+        If edge_list = True, returns a list of the edges.
+        '''
         if edge_list:
-            return list(zip(self._path[n][:-1], self._path[n][1:]))
+            return list(zip(self.paths[n][:-1], self.paths[n][1:]))
         else:
-            return self._path[n]
-
-    def __getitem__(self, key):
-        return list(zip(self._nodes, self._edges))[key]
-
-    def __iter__(self):
-        return iter(zip(self._nodes, self._edges))
-
-    def __call__(self, key):
-        return self._path[key]
+            return self.paths[n]
